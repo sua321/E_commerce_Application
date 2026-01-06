a6ecdb77be021ce1959c3c55748e6a78d76c40cc
sua321 <sugasugatheepan@gmail.com>
2026-01-06 19:08:16 +0530

    6th: added some httpSecurity features and some authenticated features didn't work(/userCart/{id}), non authenticated features works

--- src/main/java/com/me/e_commerce_application/controllers/ShowItemController.java
+++ src/main/java/com/me/e_commerce_application/controllers/ShowItemController.java
@@ -5,6 +5,8 @@ import com.me.e_commerce_application.dto.showingDtos.ItemFullDto;
 import com.me.e_commerce_application.services.ItemService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -12,15 +14,16 @@ import java.util.List;
 @AllArgsConstructor
 
 @RestController
+@RequestMapping("/item")
 public class ShowItemController {
     private final ItemService itemService;
 
-    @GetMapping("/1")
+    @GetMapping("/allItems")
     public List<ItemShortDetailsDao> getAllItems(){
         return itemService.showAllItems();
     }
-    @GetMapping("/2")
-    public ItemFullDto getOneItem(String id){
+    @GetMapping("/oneItem/{:id}")
+    public ItemFullDto getOneItem(@PathVariable String  id){
         return ItemService.showOneItem(id);
     }
 }
