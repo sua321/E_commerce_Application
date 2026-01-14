0d39a6582548a952641cc32d33015adb8b0be7a1
sua321 <sugasugatheepan@gmail.com>
2026-01-14 11:25:44 +0530

    new initial

---
+++ E_commerce_app_vendor_api/src/main/java/com/me/e_commerce_app_vendor_api/ECommerceAppVendorApiApplication.java
@@ -0,0 +1,13 @@
+package com.me.e_commerce_app_vendor_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommerceAppVendorApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceAppVendorApiApplication.class, args);
+    }
+
+}
---
+++ E_commerce_app_vendor_api/src/test/java/com/me/e_commerce_app_vendor_api/ECommerceAppVendorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.me.e_commerce_app_vendor_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceAppVendorApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
